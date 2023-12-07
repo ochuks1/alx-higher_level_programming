9642c043f4a7d9f85769160c3be10ccb29da30f7
ochuks1 <cochuks@gmail.com>
2023-12-06 22:34:44 -0800

    Tasks 4 and 5

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
-#!/usr/bin/python3.8
-import hidden_4
+#!/usr/bin/python3
 if __name__ == "__main__":
+    import hidden_4
+    # Print sorted name from directory
     for name in sorted(dir(hidden_4)):
-        if name[0:2] != "__":
-            print(name)
+        # print only names that do not start with __
+        if name[:2] != '__':
+            print("{}".format(name))
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 from variable_load_5 import a
 
+
+def find_var():
+    print(a)
+
+
 if __name__ == "__main__":
-print(a)
+    find_var()
