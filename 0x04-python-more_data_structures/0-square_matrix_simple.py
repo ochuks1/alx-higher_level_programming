581a572e7c062e1193dd7210e3f9922b02a78d6d
ochuks1 <cochuks@gmail.com>
2023-12-06 20:39:30 -0800

    Tasks 0 to 3

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    return [[x ** 2 for x in row] for row in matrix]
+
+# Example usage:
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
