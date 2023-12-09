c112ec0943e9c62d0105cbf6238fb082449695ba
ochuks1 <cochuks@gmail.com>
2023-12-09 05:51:25 -0800

    Task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return [replace if x == search else x for x in my_list]
-
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
-print(new_list)
-print(my_list)
-
+    return [replace if search == n else n for n in my_list]
