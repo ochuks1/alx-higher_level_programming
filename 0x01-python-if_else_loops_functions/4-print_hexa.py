74c6ae08e06dd9e43df8fde96281d7057057c0ac
ochuks1 <cochuks@gmail.com>
2023-12-09 07:45:31 -0800

    Task 4 and chmod 1 to 4

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for number in range(99 + 1):
-    print("{} = {}".format(number, hex(number)), end="\n" if number == 99 else ", ")
+for num in range(0, 99):
+    print("{:d} = 0x{:x}".format(num, num))
