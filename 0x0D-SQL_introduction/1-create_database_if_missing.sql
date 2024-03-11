e8f6687a05ff5013d06a8e90e1a6fc3c7e18a3b5
ochuks1 <cochuks@gmail.com>
2024-03-11 08:44:32 -0700

    Correction

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
---Script that creates a hbtn_0c_0 database in MySQL server
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+-- Script that creates a hbtn_0c_0 database in MySQL server
+CREATE DATABASE IF NOT EXISTS 'hbtn_0c_0';
